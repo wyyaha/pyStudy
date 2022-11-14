4a24d1edeb97dd37fe66e9bf05e7f9905c261e08
harrwa01 <harrwa01@mmdstarter01.onmicrosoft.com>
2022-11-14 17:50:30 +0800

    异常

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 # fr = open("C:/wyy/test.txt", 'r', encoding="UTF-8")
-fy = open("C:/wyy/yy.txt", 'w', encoding="UTF-8")
+# fy = open("C:/wyy/yy.txt", 'w', encoding="UTF-8")
 
 # for line in fr:
 #     line1 = line.strip()
 #     if line1.split("，")[2] == '语文':
 #         continue
 #     fw.write(line)
-fy.write("1")
-# fw.write("\n")
-fy = open("C:/wyy/yy.txt", 'w', encoding="UTF-8")
-fy.write("2")
+
 # fr.close()
-fy.close()
+# fy.close()
+
+try:
+    1 / 0
+except Exception as e:
+    print(f"出现异常：{e}")
