f701f6d3027f3705e6d129312182cc80081b747d
wyyaha <1193826051@qq.com>
2022-11-19 21:09:19 +0800

    模板文件

--- djangoStudy/firstProject/app01/views.py
+++ djangoStudy/firstProject/app01/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("欢迎使用django")
+
+
+def user_list(request):
+    return render(request, 'user_list.html')
+
+def user_add(request):
+    return HttpResponse('添加用户')
--- djangoStudy/firstProject/firstProject/urls.py
+++ djangoStudy/firstProject/firstProject/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ..app01 import views
+from app01 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index),
+    path('user/list/', views.user_list),
+    path('user/add/', views.user_add),
 ]
