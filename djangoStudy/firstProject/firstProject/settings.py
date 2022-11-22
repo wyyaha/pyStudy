9c7d2a004c936e0568f46999857aa6dbdd0adf36
wyyaha <1193826051@qq.com>
2022-11-22 15:47:18 +0800

    orm操作数据表

--- djangoStudy/firstProject/firstProject/settings.py
+++ djangoStudy/firstProject/firstProject/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'firstProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wyy_day5',
+        'USER': 'wyy',
+        'PASSWORD': 'Pactera@123',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
